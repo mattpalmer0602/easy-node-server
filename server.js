44bec1f876c55be8db8096b7ec5b8b5de99d140c
mattpalmer0602 <mattpalmer0602@gmail.com>
2026-08-27 02:17:21 -0700

    Show Hello World without the secret

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const http = require("http");
 
 const PORT = process.env.PORT || 3000;
-const secret = process.env.APP_SECRET || "";
-const secretStatus = secret
-  ? `set (ends with ${secret.slice(-4)})`
-  : "NOT SET";
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(`Hello from my Node server.\nSecret: ${secretStatus}\n`);
+  res.end("Hello World!\n");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
